c721444627d42296e623ab4a95ba9cdcbf865ded
phisonkha <phison.khankhang@allianz.com>
2020-08-03 15:50:04 +0700

    3 first test case with readme.md

---
+++ src/main/java/CircularBuffer.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+
+public class CircularBuffer {
+    private int bufferSize = 10;
+    private String[] buffer;
+    private int readPointer;
+    private int writePointer;
+
+    public CircularBuffer() {
+        this.buffer = new String[10];
+    }
+
+    public boolean isEmpty() {
+        return bufferSize == 10;
+    }
+
+    public void writeData(String input) {
+        bufferSize--;
+        this.buffer[writePointer++] = input;
+    }
+
+    public boolean isFull() {
+        return bufferSize == 0;
+    }
+
+    public String readData() {
+        return this.buffer[readPointer++];
+    }
+}
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         return "Hello World";
     }
     public String sayHi(String name){
+        //https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
         String result = name.substring(0,1).toUpperCase()+name.substring(1);
         return "Hello "+ result;
     }
